ed64efeb37ea025db0f11ff3b12a59f16140e2d6
王红兴 <wanghongxing@pingxx.com>
2018-09-30 22:09:10 +0800

    Update

--- test_01.py
+++ test_01.py
@@ -5,20 +5,24 @@ __author__ = 'wanghongxing'
 from selenium import webdriver
 import unittest
 import time
+import os
 
 
 class OpenBrowser(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome("chromedriver")
+        self.driver = webdriver.PhantomJS
 
     def test_open_bing(self):
         self.driver.get("https://cn.bing.com/")
         time.sleep(5)
         assert "Bing" in self.driver.title
+        print os.path
 
     def tearDown(self):
         self.driver.close()
+
+
 
 
 if __name__ == "__main__":
