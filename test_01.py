7c75f4b1fbf2bfc1249786ca284954d184bed157
王红兴 <wanghongxing@pingxx.com>
2018-10-02 19:00:33 +0800

    Update

--- test_01.py
+++ test_01.py
@@ -11,10 +11,13 @@ import os
 class OpenBrowser(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path="chromedriver")
+        #self.driver = webdriver.Chrome(executable_path="chromedriver")
+        #self.driver = webdriver.PhantomJS(executable_path="/Users/wanghongxing/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs")
+        self.driver = webdriver.PhantomJS(executable_path="./phantomjs")
 
     def test_open_bing(self):
         "test description hongstarw"
+
         self.driver.get("https://cn.bing.com/")
         time.sleep(5)
         assert "Bing" in ("Bing")
