c547230388ba8af48a29ce36d45e835c05efd340
王红兴 <wanghongxing@pingxx.com>
2018-10-02 15:00:18 +0800

    update

--- runtests.py
+++ runtests.py
@@ -6,16 +6,30 @@ from unittest import  TextTestResult
 from test_01 import OpenBrowser
 import HTMLTestRunner
 
-def suite():
-    suite = TestSuite()
-    suite.addTest(OpenBrowser('test_open_bing'))
-    file = open("result.html","wb")
-    #test_result = TextTestResult(stream=file, descriptions="test", verbosity=2)
-    runner = HTMLTestRunner.HTMLTestRunner(stream=file, title='api test result', description='test result')
-    #suite.run(result=test_result)
-    #print test_result
-    runner.run(suite)
+import nose
+from proboscis import TestProgram
 
+# def suite():
+#     suite = TestSuite()
+#     suite.addTest(OpenBrowser('test_open_bing'))
+#     file = open("result.html","wb")
+#     #test_result = TextTestResult(stream=file, descriptions="test", verbosity=2)
+#     runner = HTMLTestRunner.HTMLTestRunner(stream=file, title='api test result', description='test result')
+#     #suite.run(result=test_result)
+#     #print test_result
+#     runner.run(suite)
+
+
+
+
+def run_tests():
+    nose.run()
+
+
+def run_test_prob():
+    import test_01
+
+    TestProgram().run_and_exit()
 
 if __name__ == "__main__":
-    suite()
+    run_tests()
